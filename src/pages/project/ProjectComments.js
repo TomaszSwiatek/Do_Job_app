ab4f9c51d77cc29b9e1c3d32fbc2c11cba2f6581
Tomasz Swiatek <tomasswiatek@gmail.com>
2022-03-12 19:40:36 +0100

    project comments logic&layout

--- src/pages/project/ProjectComments.js
+++ src/pages/project/ProjectComments.js
@@ -1,10 +1,13 @@
 import { useState } from 'react'
+import Avatar from '../../components/Avatar'
 import { timestamp } from '../../firebase/config'
 import { useAuthContext } from '../../hooks/useAuthContext'
+import { useFirestore } from '../../hooks/useFirestore'
 
-export default function ProjectComments() {
+export default function ProjectComments({ project }) {
     const [newComment, setNewComment] = useState('')
     const { user } = useAuthContext()
+    const { updateDocument, response } = useFirestore('projects')
     const handleSubmit = async (e) => {
         e.preventDefault()
         const commentToAdd = {
@@ -15,10 +18,34 @@ export default function ProjectComments() {
             id: Math.random()
         }
         console.log(commentToAdd)
+        // we only updating the particular property we want - here "commnents" prop:
+        await updateDocument(project.id,
+            { comments: [...project.comments, commentToAdd] })
+
+        if (!response.error) {
+            setNewComment('')
+        }
     }
     return (
         <div className='project-comments'>
             <h4>Project Comments</h4>
+
+            <ul>
+                {project.comments.length > 0 && project.comments.map(comment => (
+                    <li key={comment.id}>
+                        <div className="comment-author">
+                            <Avatar src={comment.photoURL} />
+                            <p>{comment.displayName}</p>
+                        </div>
+                        <div className='comment-date'>
+                            <p>date here</p>
+                        </div>
+                        <div className="comment-content">
+                            <p>{comment.content}</p></div>
+                    </li>
+                ))}
+            </ul>
+
             <form className='add-comment' onSubmit={handleSubmit}>
                 <label>
                     <span>Add new comment:</span>
