b3c1ccd057ada05b8de8322707601ccb3ed42624
Tomasz Swiatek <tomasswiatek@gmail.com>
2022-03-12 20:11:23 +0100

    deleting projects by user created it

--- src/pages/project/ProjectSummary.js
+++ src/pages/project/ProjectSummary.js
@@ -1,24 +1,43 @@
 import Avatar from '../../components/Avatar'
+import { useFirestore } from '../../hooks/useFirestore'
+import { useAuthContext } from '../../hooks/useAuthContext'
+import { useNavigate } from 'react-router-dom'
 
 
 function ProjectSummary({ project }) {
+    const { deleteDocument } = useFirestore('projects')
+    const handleClick = () => {
+        deleteDocument(project.id)
+        navigate('/')
+
+    }
+    const { user } = useAuthContext()
+    const navigate = useNavigate();
     return (
-        <div className='project-summary'>
-            <h2 className="page-title">{project.name}</h2>
-            <p className="due-date">
-                Project due by{project.dueDate.toDate().toDateString()}
-            </p>
-            <p className="details">
-                {project.details}
-            </p>
-            <h4>Project is assigned to:</h4>
-            <div className="assigned-users">
-                {project.assignedUsersList.map(user => (
-                    <div key={user.photoURL} >
-                        <Avatar src={user.photoURL} />
-                    </div>
-                ))}
-            </div>
+        <div>
+            <div className='project-summary'>
+                <h2 className="page-title">{project.name}</h2>
+                <p>By {project.createdBy.displayName}</p>
+                <p className="due-date">
+                    Project due by{project.dueDate.toDate().toDateString()}
+                </p>
+                <p className="details">
+                    {project.details}
+                </p>
+                <h4>Project is assigned to:</h4>
+                <div className="assigned-users">
+                    {project.assignedUsersList.map(user => (
+                        <div key={user.photoURL} >
+                            <Avatar src={user.photoURL} />
+                        </div>
+                    ))}
+                </div>
+            </div >
+            {
+                project.createdBy.id === user.uid && <button
+                    className="btn"
+                    onClick={handleClick}>Mark as Complete</button>
+            }
         </div>
     )
 }
